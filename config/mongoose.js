e8ac5d6f3e04cce94491c138a19aee132b60ed56
Mithilesh <mithileshmarekar01@gmail.com>
2023-09-05 18:31:19 +0530

    Connected to mongodb

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 // connecting mongoose to its default server and ecommerceDB
-mongoose.connect('mongodb://0.0.0.0:27017', {
-    useNewUrlParser: true
-});
+// mongoose.connect('mongodb://0.0.0.0:27017', {
+
+mongoose.connect("mongodb+srv://mithileshmarekar01:Wz8V39O64XjyFjxP@e-commerce-api.x97h4ln.mongodb.net/");
+    useNewUrlParser: true;
+    
 
 const db = mongoose.connection;
 
